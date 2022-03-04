1bc9ed44f8106233e86c38d1a0184f1fef3f86ac
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-03-04 11:59:02 +0000

    メニューボタン

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -3,6 +3,7 @@ import {
   BoxProps,
   Button,
   Heading,
+  useBoolean,
 } from "@chakra-ui/react";
 import {
   motion,
@@ -16,10 +17,20 @@ export const MotionBox = motion<BoxProps | Transition>(Box);
 const Test: VFC = () => {
   const control = useAnimation();
   const control2 = useAnimation();
+  const [flag, setFlag] = useBoolean(false);
 
   const onClickAnime = () => {
     control.start({ rotate: -45, top: "50%" });
     control2.start({ rotate: 45, top: "50%" });
+  };
+
+  const variants = {
+    closed: { rotate: 0, top: "40%" },
+    open: { rotate: -45, top: "50%" },
+  };
+  const variants2 = {
+    closed: { rotate: 0, top: "60%" },
+    open: { rotate: 45, top: "50%" },
   };
 
   return (
@@ -45,6 +56,52 @@ const Test: VFC = () => {
           background={"gray.400"}
           transition={{ duration: 1 }}
           animate={control2}
+        />
+      </Button>
+      <Button onClick={onClickAnime} bg={"cyan.100"}>
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          top={"40%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 1 }}
+          animate={control}
+        />
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          bottom={"40%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 1 }}
+          animate={control2}
+        />
+      </Button>
+      <Button onClick={setFlag.toggle}>
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          top={"40%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 0.5 }}
+          animate={flag ? "open" : "closed"}
+          variants={variants}
+        />
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          top={"60%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 0.5 }}
+          animate={flag ? "open" : "closed"}
+          variants={variants2}
         />
       </Button>
     </>
