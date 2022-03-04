aca193d6ee9ef147474ef32f29158fd3a2909e72
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-03-04 09:00:17 +0000

    二本線から×印に

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -3,57 +3,49 @@ import {
   BoxProps,
   Button,
   Heading,
-  Icon,
-  Input,
 } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import { useRef, useState, VFC } from "react";
-import { ImFilePicture } from "react-icons/im";
+import {
+  motion,
+  Transition,
+  useAnimation,
+} from "framer-motion";
+import { VFC } from "react";
 
-export const MotionBox = motion<BoxProps>(Box);
+export const MotionBox = motion<BoxProps | Transition>(Box);
+
 const Test: VFC = () => {
-  const inputEl = useRef<HTMLInputElement>(null);
-  const inputEl2 = useRef<HTMLInputElement>(null);
-  const [name, setName] = useState("");
-  const [fileName, setFileName] = useState<string>();
+  const control = useAnimation();
+  const control2 = useAnimation();
+
+  const onClickAnime = () => {
+    control.start({ rotate: -45, top: "50%" });
+    control2.start({ rotate: 45, top: "50%" });
+  };
 
   return (
     <>
       <Heading>テスト</Heading>
-      <MotionBox
-        h={"40px"}
-        w={"40px"}
-        bg={"red.300"}
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-        }}
-      />
-      <Input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        ref={inputEl}
-      />
-      <Heading>名前: {name}</Heading>
-      <Button
-        onClick={() =>
-          inputEl.current && (inputEl.current.value = "")
-        }
-      >
-        フォーカスを当てる
-      </Button>
-      <Input
-        type={"file"}
-        ref={inputEl2}
-        hidden
-        onChange={() =>
-          setFileName(inputEl2.current?.files?.[0]?.name)
-        }
-      />
-      <Heading>{fileName}</Heading>
-      <Button onClick={() => inputEl2.current?.click()}>
-        <Icon as={ImFilePicture} />
+      <Button onClick={onClickAnime} bg={"cyan.100"}>
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          top={"40%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 1 }}
+          animate={control}
+        />
+        <MotionBox
+          display="inline-block"
+          position={"absolute"}
+          bottom={"40%"}
+          h={"2px"}
+          w={"60%"}
+          background={"gray.400"}
+          transition={{ duration: 1 }}
+          animate={control2}
+        />
       </Button>
     </>
   );
